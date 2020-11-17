f65d19cf5df8d9b0f0707c1f7978d253c3ab7893
sanisdon <74405143+sanisdon@users.noreply.github.com>
2020-11-17 17:30:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+
+    res.send('hello world');
+
+});
+
+
+app.get('/testing', (req, res) => {
+
+    res.send('testing route vs');
+
+});
+
+app.listen(5000);
+console.log('this app run in 5000 port');
